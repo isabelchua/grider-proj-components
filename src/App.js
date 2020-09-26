eff5b342664cf8cfa2ea52f7c077e156cd2f02f3
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-25 17:51:39 -0700

    component props ex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
+import faker from "faker";
 
 import "./App.css";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 function App() {
 	return (
 		<div className="ui container comments">
-			<CommentDetail />
+			<ApprovalCard>
+				<h4>Warning!</h4>
+				<p>Are you sure?</p>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="sam"
+					timeAgo="Today at 6:00pm"
+					comment="Hello"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="alex"
+					timeAgo="Yesterday at 3:00pm"
+					comment="Hiiii"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="jane"
+					timeAgo="Yesterday at 4:00pm"
+					comment="Hey"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 }
--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,20 @@
 import React from "react";
-import faker from "faker";
 
-function CommentDetail() {
+function CommentDetail(props) {
 	return (
 		<div className="comment">
 			<a href="/" className="avatar">
-				<img src={faker.image.avatar()} alt="avatar" />
+				<img src={props.avatar} alt="avatar" />
 			</a>
 			<div className="content">
 				<a href="/" className="author">
-					Sam
+					{props.author}
 				</a>
 
 				<div className="metadata">
-					<span className="date">Today at 6:00pm</span>
+					<span className="date">{props.timeAgo}</span>
 				</div>
-				<div className="text">nice blog post</div>
+				<div className="text">{props.comment}</div>
 			</div>
 		</div>
 	);
